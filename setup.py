775116d5aeaa205261621fdc128d0459c6873a7e
Ilya Skryagin <infox182@gmail.com>
2022-12-21 02:22:48 +0300

    Black formatting

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-        name="test-rec-system",
-        version="1.0.0",
-        packages=find_packages(include = ['rec-system']),
-        description="test package",
-        # long_description=long_description,
-        url="https://github.com/infox182/ml-rec-systems",
-        author="Ilya Skryagin"
-        )
+    name="test-rec-system",
+    version="1.0.0",
+    packages=find_packages(include=["rec-system"]),
+    description="test package",
+    # long_description=long_description,
+    url="https://github.com/infox182/ml-rec-systems",
+    author="Ilya Skryagin",
+)
--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import os
 
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-os.environ['MKL_NUM_THREADS'] = '1'
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+os.environ["MKL_NUM_THREADS"] = "1"
 
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 import pandas as pd
 import numpy as np
@@ -13,8 +14,7 @@ import random
 import time
 from joblib import dump
 
-from rectools.models import (PopularModel,
-                             LightFMWrapperModel)
+from rectools.models import PopularModel, LightFMWrapperModel
 from rectools import Columns
 from rectools.dataset import Dataset
 
@@ -23,7 +23,7 @@ from pathlib import Path
 from lightfm import LightFM
 
 
-#set seed
+# set seed
 RANDOM_STATE = 42
 np.random.seed(RANDOM_STATE)
 random.seed(RANDOM_STATE)
@@ -31,21 +31,25 @@ random.seed(RANDOM_STATE)
 DATA_PATH = Path("experiments/data_original")
 
 
-users = pd.read_csv(DATA_PATH / 'users.csv')
-items = pd.read_csv(DATA_PATH / 'items.csv')
-interactions = pd.read_csv(DATA_PATH / 'interactions.csv')
+users = pd.read_csv(DATA_PATH / "users.csv")
+items = pd.read_csv(DATA_PATH / "items.csv")
+interactions = pd.read_csv(DATA_PATH / "interactions.csv")
 
-Columns.Datetime = 'last_watch_dt'
-interactions.drop(interactions[interactions[Columns.Datetime].str.len() != 10].index, inplace=True)
-interactions[Columns.Datetime] = pd.to_datetime(interactions[Columns.Datetime], format='%Y-%m-%d')
+Columns.Datetime = "last_watch_dt"
+interactions.drop(
+    interactions[interactions[Columns.Datetime].str.len() != 10].index, inplace=True
+)
+interactions[Columns.Datetime] = pd.to_datetime(
+    interactions[Columns.Datetime], format="%Y-%m-%d"
+)
 max_date = interactions[Columns.Datetime].max()
-interactions[Columns.Weight] = np.where(interactions['watched_pct'] > 10, 3, 1)
+interactions[Columns.Weight] = np.where(interactions["watched_pct"] > 10, 3, 1)
 
 train = interactions.copy()
 
 train.drop(train.query("total_dur < 300").index, inplace=True)
 
-users.fillna('Unknown', inplace=True)
+users.fillna("Unknown", inplace=True)
 users = users.loc[users[Columns.User].isin(train[Columns.User])].copy()
 
 user_features_frames = []
@@ -59,7 +63,9 @@ user_features = pd.concat(user_features_frames)
 
 items = items.loc[items[Columns.Item].isin(train[Columns.Item])].copy()
 
-items["genre"] = items["genres"].str.lower().str.replace(", ", ",", regex=False).str.split(",")
+items["genre"] = (
+    items["genres"].str.lower().str.replace(", ", ",", regex=False).str.split(",")
+)
 genre_feature = items[["item_id", "genre"]].explode("genre")
 genre_feature.columns = ["id", "value"]
 genre_feature["feature"] = "genre"
@@ -79,26 +85,26 @@ dataset = Dataset.construct(
 )
 
 
-
 best_light_fm_params = {
-    'loss': 'warp',
-    'no_components': 100,
-    'learning_rate': 0.0066594231278431434,
-    'user_alpha': 0.0005203512950757777,
-    'item_alpha': 0.0003116105897809789,
-    'random_state': 42,
+    "loss": "warp",
+    "no_components": 100,
+    "learning_rate": 0.0066594231278431434,
+    "user_alpha": 0.0005203512950757777,
+    "item_alpha": 0.0003116105897809789,
+    "random_state": 42,
 }
 
 N_EPOCHS = 1
 NUM_THREADS = 8
-best_model = LightFMWrapperModel(LightFM(**best_light_fm_params),
-                                        epochs=N_EPOCHS, num_threads=NUM_THREADS)
+best_model = LightFMWrapperModel(
+    LightFM(**best_light_fm_params), epochs=N_EPOCHS, num_threads=NUM_THREADS
+)
 best_model.fit(dataset)
 
-#популярные айтемы для холодных пользователей
+# популярные айтемы для холодных пользователей
 popular_model = PopularModel()
 popular_model.fit(dataset)
 
-dump(best_model,"best_models/main_model.joblib")
-dump(popular_model,"best_models/popular_model.joblib")
-dump(dataset,"best_models/dataset.joblib")
+dump(best_model, "best_models/main_model.joblib")
+dump(popular_model, "best_models/popular_model.joblib")
+dump(dataset, "best_models/dataset.joblib")
